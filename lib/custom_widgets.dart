042d093a7fd859ce7198a26df346ada7189abe44
jimmyriguelle <jimmyriguelle@hotmail.com>
2021-11-18 04:29:05 +0100

    chat page

--- lib/custom_widgets.dart
+++ lib/custom_widgets.dart
@@ -1,4 +1,5 @@
 export 'package:flutter/material.dart';
+export 'package:chat1/constants.dart';
 export 'package:chat1/screens/welcome_screen.dart';
 export 'package:chat1/screens/login_screen.dart';
 export 'package:chat1/screens/registration_screen.dart';
--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -11,6 +11,46 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        leading: null,
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.close),
+          ),
+        ],
+        title: const Text('⚡️Chat'),
+        backgroundColor: Colors.lightBlueAccent,
+      ),
+      body: SafeArea(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            decoration: kMessageContainerDecoration,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                  child: TextField(
+                    onChanged: (value) {},
+                    decoration: kMessageTextFieldDecoration,
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'Send',
+                    style: kSendButtonTextStyle,
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      )),
+    );
   }
 }
