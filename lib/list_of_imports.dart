6d81d727f0264bd60bc02119a6296e7a7f60e67f
jimmyriguelle <jimmyriguelle@hotmail.com>
2021-11-19 05:43:09 +0100

    refactor

--- lib/list_of_imports.dart
+++ lib/list_of_imports.dart
@@ -1,12 +1,12 @@
 export 'package:flutter/material.dart';
-export 'package:chat1/constants.dart';
+export 'package:chat1/user_constants.dart';
 export 'package:animated_text_kit/animated_text_kit.dart';
 export 'package:loading_overlay/loading_overlay.dart';
 export 'package:chat1/screens/welcome_screen.dart';
 export 'package:chat1/screens/login_screen.dart';
 export 'package:chat1/screens/registration_screen.dart';
 export 'package:chat1/screens/chat_screen.dart';
-export 'package:chat1/components/rounded_button.dart';
+export 'package:chat1/user_widgets/rounded_button.dart';
 export 'package:firebase_core/firebase_core.dart';
 export 'package:firebase_auth/firebase_auth.dart';
 export 'package:cloud_firestore/cloud_firestore.dart';
